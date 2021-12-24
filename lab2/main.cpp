ee8add992f32eb358d70e9874f9bb14a5fd996d5
glebbelay <glebbelay@gmail.com>
2021-12-24 23:49:07 +0300

    laba2

---
+++ lab2/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+
+float Z1(int a)
+{
+    return (cos(a) + sin(a) + cos(3*a) + sin(3*a));
+}
+
+float Z2(int a)
+{
+    double Pi;
+    Pi = 3.14;
+    return (2 * sqrt(2) * cos(a) * sin((Pi/4) + (2*a)));
+}
+
+int main()
+{
+
+    int a;
+    scanf("%d", &a);
+
+    printf("Z1 = %.2f\n", Z1(a));
+    printf("Z2 = %.2f\n", Z2(a));
+
+    return 0;
+}
